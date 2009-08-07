93b0421bc3d8d09bc224fb5a91a9fe99cf717caf
Paul Elliott <pelliott@red-line-it.com>
2009-08-06 21:34:49 -0400

    Added a test for invoking the curtain without a loader_image.

--- spec/jquery-curtain_spec.js
+++ spec/jquery-curtain_spec.js
@@ -1,8 +1,11 @@
 Screw.Unit(function() {
   describe("jquery-curtain", function() {
-    before(function() { $("#container").curtain(); });
+    //Clean up the curtain after each run.
     after(function() { $("#container").curtain("remove"); });
+
     describe('when curtain is applied with the default options', function() {
+      before(function() { $("#container").curtain(); });
+
       it("should have the curtain div placed over it", function() {
         expect($("#container > .jquery-curtain").length).to(equal, 1);
       });
@@ -13,6 +16,14 @@ Screw.Unit(function() {
 
       it("should no longer exist", function() {
         expect($("#container > .jquery-curtain").length).to(equal, 0);
+      });
+    });
+    
+    describe('when the curtain is invoked without an image', function() {
+      before(function() { $("#container").curtain({ loader_image: '' }); });
+      
+      it("should not have an image inside the container", function() {
+        expect($("#container img").length).to(equal, 0);
       });
     });
   });
