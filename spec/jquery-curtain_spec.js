6debd549809587e2e64ad23a2aeb0c4b189200e8
Paul Elliott <pelliott@red-line-it.com>
2009-08-17 22:17:45 -0400

    Added a test for curtaining absolutely positioned elements. Added a test for getting the curtain of an element that doesn't exist. Updated the existing specs to use the "get" functionality.

--- spec/jquery-curtain_spec.js
+++ spec/jquery-curtain_spec.js
@@ -7,7 +7,7 @@ Screw.Unit(function() {
       before(function() { $("#container").curtain(); });
 
       it("should have the curtain div placed over it", function() {
-        expect($("#container > .jquery-curtain").length).to(equal, 1);
+        expect($("#container").curtain('get').length).to(equal, 1);
       });
     });
 
@@ -15,15 +15,29 @@ Screw.Unit(function() {
       before(function() { $("#container").curtain().curtain("remove"); });
 
       it("should no longer exist", function() {
-        expect($("#container > .jquery-curtain").length).to(equal, 0);
+        expect($("#container").curtain('get').length).to(equal, 0);
       });
     });
     
     describe('when the curtain is invoked without an image', function() {
       before(function() { $("#container").curtain({ loader_image: '' }); });
       
-      it("should not have an image inside the container", function() {
-        expect($("#container img").length).to(equal, 0);
+      it("should not have an image inside the curtain", function() {
+        expect($("#container").curtain('get').find("img").length).to(equal, 0);
+      });
+    });
+    
+    describe("when the curtain is placed over an absolutely positioned element", function() {
+      before(function() { $("#test_abs_pos").curtain(); });
+      
+      it("should display in the correct location", function() {
+        expect($("#test_abs_pos").curtain('get').offset()).to(equal, $("#test_abs_pos").offset());
+      });
+    });
+    
+    describe("when requesting the curtain for an element that doesn't exist", function() {
+      it("should return an empty jQuery object", function() {
+        expect($("#princess_leia").curtain('get').length).to(equal, 0);
       });
     });
   });
